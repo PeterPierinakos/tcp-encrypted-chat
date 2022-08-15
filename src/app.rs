092aea812a6b53917310df04114c48a5f0391a42
PeterPierinakos <peterpierinakos-business@protonmail.com>
2022-08-15 09:26:38 +0000

    Basic server for handling TCP streams and --with-logs argument

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,67 @@
 use std::net::SocketAddr;
+use std::io::{self, Read, Write};
+use std::net::TcpListener;
+use std::thread;
+use std::sync::mpsc::{self, Receiver, Sender};
 use std::env;
 
+/// The main application's function. it is responsible for handling the TCP listener, the incoming
+/// streams and the sending user's messages.
 pub fn init(port: u16, peer_addr: SocketAddr) {
+    let (message_tx, message_rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+    start_server(message_tx, peer_addr, port);
+    log::info!("Server started");
 
+    // Indefinitely locks standard input so that the program doesn't end.
+    let _ = io::stdin().lock();
 }
 
-pub fn handle_other_arguments(args: Vec<String>) {
+pub fn start_server(message_tx: Sender<String>, peer_addr: SocketAddr, port: u16) {
+    thread::spawn(move || {
+        let listener = TcpListener::bind(format!("0.0.0.0:{port}")).expect("Failed listening to given port. Maybe the program doesn't have the required privileges?");
+
+        for stream in listener.incoming() {
+            let mut stream = match stream {
+                Ok(stream) => stream,
+                Err(_) => {
+                    log::warn!("Something went wrong whilst getting stream, skipping. Possibly invalid data?");
+                    continue;
+                }
+            };
+
+            let stream_peer_addr = match stream.peer_addr() {
+                Ok(peer_addr) => peer_addr,
+                Err(_) => continue,
+            };
+
+            if stream_peer_addr != peer_addr {
+                log::warn!("Permission denied ({stream_peer_addr})");
+                if let Err(_) = stream.write(format!("Permission denied.\n\nYour socket address doesn't match the given peer's address.\nYour socket address: {stream_peer_addr}\nGiven peer's address: {peer_addr}").as_bytes()) {
+                    log::warn!("Failed writing error to TCP stream, skipping.");
+                };
+                continue;
+            }
+
+            println!("Connection successfully established with peer ({stream_peer_addr})");
+        }
+    });
+}
+
+/// Handles all arguments besides none. Returns true if the app should continue after the arguments
+/// are handled.
+pub fn handle_other_arguments(args: Vec<String>) -> bool {
     let first_arg = args[1].clone();
 
     match first_arg.as_str() {
         "-v" | "--version" => println!("Version: {}", env::var("CARGO_PKG_VERSION").unwrap_or("could not detect Cargo version. Make sure you are running the program with the Rust's Cargo package manager.".to_string())),
         "-h" | "--help" => println!("There is no savior."),
+        "--with-logs" => {
+            env::set_var("RUST_LOG", "debug");
+            env_logger::init();
+            return true;
+        }
         _ => println!("Invalid first argument provided."),
     }
+
+    false
 }
--- src/bin/tec.rs
+++ src/bin/tec.rs
@@ -3,6 +3,35 @@ use std::str::FromStr;
 use std::net::SocketAddr;
 use std::io::{self, BufRead, Write};
 
+fn handle_input() -> anyhow::Result<()> {
+    println!("Enter port the server should listen to for incoming TCP streams");
+    print!("> ");
+
+    io::stdout().flush()?;
+    let mut stdin = io::stdin().lock();
+    let mut buffer_port = String::new();
+    stdin.read_line(&mut buffer_port)?;
+    let port = match buffer_port.trim().parse::<u16>() {
+        Ok(port) => port,
+        Err(_) => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::InvalidData, "Invalid port given. The port can be 1 - 65535."))),
+    };
+
+    println!("Enter peer socket address");
+    print!("> ");
+    io::stdout().flush()?;
+    let mut buffer_addr = String::new();
+    stdin.read_line(&mut buffer_addr)?;
+    let peer_addr = match SocketAddr::from_str(&buffer_addr.trim()) {
+        Ok(addr) => addr,
+        Err(_) => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::InvalidData, "Invalid socket address given. Example of a valid socket address: \"127.0.0.1:5542\"."))),
+    };
+
+    log::info!("Input is OK, starting server...");
+    app::init(port, peer_addr);
+
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
     println!("TEC - TCP Encrypted Chat");
     println!();
@@ -11,29 +40,13 @@ fn main() -> anyhow::Result<()> {
 
     match args.len() {
         1 => {
-            println!("Enter port the server should listen to for incoming TCP streams");
-            print!("> ");
-            io::stdout().flush()?;
-            let mut stdin = io::stdin().lock();
-            let mut buffer_port = String::new();
-            stdin.read_line(&mut buffer_port)?;
-            let port = match buffer_port.parse::<u16>() {
-                Ok(port) => port,
-                Err(_) => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::InvalidData, "Invalid port given. The port can be 1 - 65535."))),
-            };
-            println!("Enter peer socket address");
-            print!("> ");
-            io::stdout().flush()?;
-            let mut stdin = io::stdin().lock();
-            let mut buffer_addr = String::new();
-            stdin.read_line(&mut buffer_addr)?;
-            let peer_addr = match SocketAddr::from_str(&buffer_addr) {
-                Ok(addr) => addr,
-                Err(_) => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::InvalidData, "Invalid socket address given. Example of a valid socket address: \"127.0.0.1:5542\"."))),
-            };
-            app::init(port, peer_addr)
+            handle_input()?;
         },
-        _ => app::handle_other_arguments(args),
+        _ => {
+            if app::handle_other_arguments(args) {
+                handle_input()?;
+            }
+        },
     }
 
     Ok(())
